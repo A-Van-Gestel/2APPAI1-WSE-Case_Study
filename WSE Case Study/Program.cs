e340beb7d585468aa218f044a92decc64eb6f34b
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-11-30 16:05:14 +0100

    Initial Commit

---
+++ WSE Case Study/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSE_Case_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

3e20a7ef10398cd75a6ea2147c0e616f53c0d630
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-12-01 22:39:09 +0100

    Updated: Github Actions - Testing

--- WSE Case Study/Program.cs
+++ WSE Case Study/Program.cs
@@ -9,6 +9,7 @@ namespace WSE_Case_Study
             Console.WriteLine("Hello World!");
             Console.WriteLine("Test for Github Actions!");
             Console.WriteLine("Test for Github Actions 123");
+            Console.WriteLine("Test for Github Actions 123");
             Console.WriteLine("Press ENTER to exit...");
             Console.ReadLine();
         }
